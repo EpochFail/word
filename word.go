b2539cda9c4880741c2e7b0fd706b02a2e45dc30
Carter Bancroft <alt234@gmail.com>
2013-09-04 01:07:12 -0800

    Add functionality to store and send statistics.

--- word.go
+++ word.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/bmizerany/pat"
 	"github.com/daaku/go.httpgzip"
@@ -16,6 +17,7 @@ func main() {
 	m.Get("/api/word", httpgzip.NewHandler(http.HandlerFunc(wordMeBro)))
 	m.Get("/api/vote/:word/up", httpgzip.NewHandler(http.HandlerFunc(upVoteMe)))
 	m.Get("/api/vote/:word/down", httpgzip.NewHandler(http.HandlerFunc(downVoteMe)))
+	m.Get("/api/stats", httpgzip.NewHandler(http.HandlerFunc(statsMe)))
 
 	http.Handle("/", m)
 
@@ -23,6 +25,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type Message struct {
+	Word   string
+	Rating int
 }
 
 func wordMeBro(w http.ResponseWriter, r *http.Request) {
@@ -34,12 +41,26 @@ func wordMeBro(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
+	var word_id int
 	var word string
-	var rating string
+	var rating int
 
-	db.QueryRow("SELECT word, rating FROM words OFFSET random()*(select max(word_id) from words) LIMIT 1").Scan(&word, &rating)
+	db.QueryRow("SELECT word_id, word, rating FROM words OFFSET random()*(select max(word_id) from words) LIMIT 1").Scan(&word_id, &word, &rating)
 
-	fmt.Fprintf(w, "{\"word\":\"%v\", \"rating\":\"%v\"}", word, rating)
+	result, err := db.Exec("insert into word_history (word_id) values ($1)", word_id)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Print(result)
+
+	m := Message{word, rating}
+	b, err := json.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(b[:]))
 }
 
 func upVoteMe(w http.ResponseWriter, r *http.Request) {
@@ -62,6 +83,54 @@ func downVoteMe(w http.ResponseWriter, r *http.Request) {
 	log.Print("Down vote for: ", word)
 }
 
+type Stats struct {
+	History  []Message
+	Top10    []Message
+	Bottom10 []Message
+}
+
+func statsMe(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	history := getStatsSlice("select w.word, w.rating from words w inner join word_history wh on w.word_id=wh.word_id order by wh.created_at desc limit 10")
+	top10 := getStatsSlice("select distinct word, rating from words order by rating desc limit 10")
+	bottom10 := getStatsSlice("select distinct word, rating from words order by rating limit 10")
+
+	s := Stats{history, top10, bottom10}
+	b, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(b[:]))
+}
+
+func getStatsSlice(queryString string) []Message {
+	// TODO: Stop opening db so often. Probably should try and ORM too.
+	db, err := sql.Open("postgres", "user=jsname password=test dbname=jsname_dev sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query(queryString)
+	if err != nil {
+		panic(err)
+	}
+
+	var stats []Message
+	for rows.Next() {
+		var _word string
+		var _rating int
+
+		rows.Scan(&_word, &_rating)
+
+		stats = append(stats, Message{_word, _rating})
+	}
+
+	return stats
+}
+
 func updateRating(word string, vote int) {
 	db, err := sql.Open("postgres", "user=jsname password=test dbname=jsname_dev sslmode=disable")
 	if err != nil {
@@ -71,8 +140,7 @@ func updateRating(word string, vote int) {
 	log.Print(word)
 	log.Print(vote)
 
-	result, err := db.Exec("insert into votes (word, vote) values (?, ?)", word, vote)
-	//result, err := db.Exec("insert into votes (word, vote) values ("test", 1)")
+	result, err := db.Exec("insert into votes (word, vote) values ($1, $2)", word, vote)
 	if err != nil {
 		panic(err)
 	}
