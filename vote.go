2983d8654efff7866bd27478213216f0039a903e
Jason Jones <jasonedwardjones@gmail.com>
2013-10-10 10:28:10 -0800

    Track vote ipaddress
    * Add column to votes to track ip address
    * Capture request remote ip address on vote

--- vote.go
+++ vote.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -12,7 +13,7 @@ func UpVoteMe(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	word := r.URL.Query().Get(":word")
-	updateRating(word, 1)
+	updateRating(word, 1, r.RemoteAddr)
 
 	log.Print("Up vote for: ", word)
 }
@@ -22,19 +23,24 @@ func DownVoteMe(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	word := r.URL.Query().Get(":word")
-	updateRating(word, -1)
+	updateRating(word, -1, r.RemoteAddr)
 
 	log.Print("Down vote for: ", word)
 }
 
-func updateRating(word string, vote int) {
+func updateRating(word string, vote int, address string) {
+	host, _, err := net.SplitHostPort(address)
+	if err != nil {
+		panic(err)
+	}
+
 	db, err := sql.Open("postgres", ConnectionString)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	_, err = db.Exec("insert into votes (word, vote) values ($1, $2)", word, vote)
+	_, err = db.Exec("insert into votes (word, vote, ipaddress) values ($1, $2, $3)", word, vote, host)
 	if err != nil {
 		panic(err)
 	}
